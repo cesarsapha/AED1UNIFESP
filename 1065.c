58975c6dd717118b061333ec137a97f741adb0f1
cesarsapha <162627241+cesarsapha@users.noreply.github.com>
2025-09-25 21:27:38 -0300

    Add files via upload

---
+++ 1065.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main () {
+
+int valores = 0, k;
+
+for (int i = 0; i < 5; i++){
+  scanf("%d", &k);
+  if (k % 2 == 0){
+    valores++;
+  }
+}
+printf("%d valores pares\n", valores);
+
+return 0;
+
+}
